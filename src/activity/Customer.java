f2c181304b60cf1857948a405526744419aeee1d
reddypramodhini@gmail.com <rahulreddyg16.dr@gmail.com>
2019-02-09 10:16:29 -0500

    changes to orderdetails

---
+++ src/activity/Customer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity;
+
+/**
+ *
+ * @author temp
+ */
+public class Customer {
+    
+    String name;
+    String location;
+    
+    public void sendOrder(){
+        
+    }
+    public void receiveOrder(){
+        
+    }
+    
+}
