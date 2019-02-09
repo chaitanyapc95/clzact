83c31b3478d8bcbcbe1a6029cce7f1ea800b29f0
1893069 <1893069@E21052.CEGEPGIM.LCL>
2019-02-09 10:21:17 -0500

    Set method

---
+++ src/activity/NormalOrder.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1893069
+ */
+public class NormalOrder{
+    
+    Date date;
+    String number;
+    public void confirm()
+    {
+        System.out.println("confirm");
+    }
+  
+    
+    
+    
+}
